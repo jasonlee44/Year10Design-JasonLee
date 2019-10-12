1e3a06154bb7e7d448b5dc642abbce01c59de193
Jason Lee <jason.c.lee@GCCYJQBTJ1WK.local>
2019-10-12 15:49:39 -0400

    added gui

--- Assignment 1/Final/AirQuality_API.py
+++ Assignment 1/Final/AirQuality_API.py
@@ -1,10 +1,9 @@
-#***-----------IMPORTANT-----------***
-#Make you have the css code downloaded
-
 import requests
+from tkinter import *
 
-def writeHTMLHead(country): #country variable: outputs to different HTML files
-    myfile = open(f"./Final/{country}.html","w")
+def writeHTMLHead(): 
+    global myfile
+    myfile = open("./Final/output.html","w")
     myfile.write("""<!DOCTYPE html>
     <html>
     <head>
@@ -12,10 +11,9 @@ def writeHTMLHead(country): #country variable: outputs to different HTML files
     <link rel = "stylesheet" type = "text/css" href = "style.css" />
     </head>
     """)
-    myfile.close()
 
-def writeHTMLNav(country):
-    myfile = open(f"./Final/{country}.html","a")
+def writeHTMLNav():
+    global myfile
     myfile.write("""    <body>
         <div class="header" id = "home">
             <h1>Air Quality by Country</h1>
@@ -23,23 +21,11 @@ def writeHTMLNav(country):
         <div class="navbar">
             <a href="#home">Home</a>
             <a href="#about">About</a>
-        <div class="dropdown">
-            <button class="dropbtn">Country 
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href="./canada.html">Canada</a>
-                <a href="./united_states.html">United States</a>
-                <a href="./china.html">China</a>
-                <a href="./england.html">England</a>
-            </div>
-        </div> 
     </div>
     """)
-    myfile.close()
 
-def writeHTMLTableTag(country):
-    myfile = open(f"./Final/{country}.html","a")
+def writeHTMLTableTag():
+    global myfile
     myfile.write("""<table>
             <tr>
                 <th>City</th>
@@ -49,8 +35,8 @@ def writeHTMLTableTag(country):
                 <th>Cautionary Statement</th>
             </tr>""")
 
-def writeHTMLTable(country,city,aqi,level,health,statement,color):
-    myfile = open(f"./Final/{country}.html","a")
+def writeHTMLTable(city,aqi,level,health,statement,color):
+    global myfile
     myfile.write(f"""
             <tr style = "background-color: {color};">
                 <td>{city}</td>
@@ -59,15 +45,15 @@ def writeHTMLTable(country,city,aqi,level,health,statement,color):
                 <td>{health}</td>
                 <td>{statement}</td>
             </tr>""")
-    myfile.close()
 
-def writeHTMLEnd(country):
-    myfile = open(f"./Final/{country}.html","a")
+def writeHTMLEnd():
+    global myfile
     myfile.write("""
            </table> 
         </body>
     </html>
     """)
+    myfile.close()
 
 #list of cities
 canada = ["Toronto","Montreal","Vancouver","Calgary","Edmonton","Ottawa","Winnipeg","Quebec","Saskatoon"]
@@ -79,15 +65,24 @@ germany = [""]
 countries_str = ["canada","united_states","china","england"] #list of countries
 countries = [canada,united_states,china,england] #list of list of cities
 
+def enter():
+    global gui_input
+    print(e1.get())
+    gui_input = e1.get()
+    main()
+    writeHTMLEnd()
+    print("Successful!")
+
 def main():
-    for i in countries_str:
-        writeHTMLHead(i) #writes the header HTML code
-        writeHTMLNav(i)
-        writeHTMLTableTag(i)
-    for i in range(len(countries)):
-        for j in countries[i]:
+    writeHTMLHead()
+    writeHTMLNav()
+    writeHTMLTableTag()
+    
+    if gui_input in countries_str:
+        for j in countries[countries_str.index(gui_input)]:
             response = requests.get(f"https://api.waqi.info/feed/{j}/?token=e6b58a3aa996953cceed553f70c04c07342ce32e")
             if (response.status_code == 200):
+                print("Status Code: 200")
                 data = response.json()
                 
                 if type(data['data']['aqi']).__name__ != "int":
@@ -120,14 +115,88 @@ def main():
                     statement = "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion"
                     color = "rgb(140, 0, 0)" #red
                     
-                writeHTMLTable(countries_str[i],j, data['data']['aqi'], level, health, statement, color)
+                writeHTMLTable(j, data['data']['aqi'], level, health, statement, color)
 
             else:
                 myfile = open("./Assignment 1/myapi.html","w")
                 myfile.write("Error has occured")
                 myfile.close()
+    else:
+        response = requests.get(f"https://api.waqi.info/feed/{gui_input}/?token=e6b58a3aa996953cceed553f70c04c07342ce32e")
+        if (response.status_code == 200):
+            print("Status Code: 200")
+            data = response.json()
 
-main()
-for i in countries_str:
-    writeHTMLEnd(i)
-print("Successful!")
+            if type(data['data']['aqi']).__name__ != "int":
+                    level = "N/A"
+                    health = "N/A"
+                    statement = "N/A"
+                    color = "grey"
+
+            elif data['data']['aqi'] <= 50:
+                level = "Good"
+                health = "Air quality is considered satisfactory, and air pollution poses little or no risk"
+                statement = "None"
+                color = "rgb(51, 211, 51)"#green
+
+            elif data['data']['aqi'] <= 100:
+                level = "Moderate"
+                health = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+                statement = "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion."
+                color = "rgb(255, 255, 100)"#yellow
+
+            elif data['data']['aqi'] <= 150:
+                level = "Unhealthy for Sensitive Groups"
+                health = "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+                statement = "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion."
+                color = "rgb(245, 125, 45)"#orange
+
+            elif data['data']['aqi'] <= 200:
+                level = "Unhealthy"
+                health = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
+                statement = "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion"
+                color = "rgb(140, 0, 0)" #red
+                
+            writeHTMLTable(data['data']['city']['name'], data['data']['aqi'], level, health, statement, color)
+        else:
+            myfile = open("./Assignment 1/myapi.html","w")
+            myfile.write("Error has occured")
+            myfile.close()
+            
+
+
+
+
+root = Tk()
+root.title("photo tagger")
+root.geometry("500x350")
+
+def test(event):
+    print(event) #ensure that this line is indented
+topframe = Frame(root,bg='blue',height='20')
+topframe.pack(fill=X)
+
+can1 = Canvas(topframe,height='20',width='20',bg="blue",highlightthickness=0)
+can1.create_line(0, 5, 20, 5,fill='white')
+can1.create_line(0, 10, 20, 10,fill='white')
+can1.create_line(0, 15, 20, 15,fill='white')
+can1.bind("<Button-1>",test ) # keyword 
+can1.grid(row=0,column=0, padx=5, pady=5)
+
+spaceframe = Frame(root,height=10) #invisible
+spaceframe.pack()
+
+frame = LabelFrame(root,borderwidth = 2, relief=RAISED, width=400,height=150)
+frame.pack()
+
+lab1 = Label(frame, text="fjkdsjkfsdjkfasdkfjkasd fdsakjfasj fkasjlf",width=40,height=5)
+lab1.grid(row=0,column=0)
+
+e1 = Entry(frame)
+e1.grid(row=1,column=0,sticky=W,padx=60)
+
+b = Button(frame, text="Sumbit",command=enter)
+b.grid(row=1, column=0,sticky=E,padx=60)
+
+root.mainloop()
+
--- Assignment 1/Final/gui.py
+++ Assignment 1/Final/gui.py
@@ -7,26 +7,30 @@ root.geometry("500x350")
 
 def test(event):
     print(event) #ensure that this line is indented
-    
-topframe = Frame(root,bg='#3bf5ff',height='20')
-topframe.pack(fill=X) # make as wide as root
-can1 = Canvas(topframe,height='20',width='20',bg="#3bf5ff",highlightthickness=0)
+topframe = Frame(root,bg='blue',height='20')
+topframe.pack(fill=X)
+
+can1 = Canvas(topframe,height='20',width='20',bg="blue",highlightthickness=0)
 can1.create_line(0, 5, 20, 5,fill='white')
 can1.create_line(0, 10, 20, 10,fill='white')
 can1.create_line(0, 15, 20, 15,fill='white')
-can1.bind("<Button-1>",test ) # keyword
-can1.pack(side=LEFT, padx=5, pady=5)
+can1.bind("<Button-1>",test ) # keyword 
+can1.grid(row=0,column=0, padx=5, pady=5)
 
-imgframe = Frame(root,borderwidth = 1.5, relief=RAISED, width=400,height=150)
-imgframe.pack(fill=None, expand=False)
-canvas = Canvas(imgframe,height=150,width=200)
-canvas.grid(row=0,column=0)
-l1 = Label(imgframe,text="Welcome to photo tagger\nA test of Material Design in Tkinter",fg="blue")
-l1.grid(row=0,column=1)
-myimage = Image.open("sky-banner.jpg")
-myimage = myimage.resize((200, 150), Image.ANTIALIAS)
-myimg = ImageTk.PhotoImage(myimage)
-canvas.create_image(0, 0, image=myimg, anchor = NW)
+spaceframe = Frame(root,height=10) #invisible
+spaceframe.pack()
+
+frame = LabelFrame(root,borderwidth = 2, relief=RAISED, width=400,height=150)
+frame.pack()
+
+lab1 = Label(frame, text="fjkdsjkfsdjkfasdkfjkasd fdsakjfasj fkasjlf",width=40,height=5)
+lab1.grid(row=0,column=0)
+
+e1 = Entry(frame)
+e1.grid(row=1,column=0,sticky=W,padx=60)
+
+b = Button(frame, text="Sumbit",command=AirQuality_API.enter)
+b.grid(row=1, column=0,sticky=E,padx=60)
 
 
 root.mainloop()
