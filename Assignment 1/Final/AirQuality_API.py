2ccc82a42aa46952c0b606e8b1563b2eba07de5a
jasonlee44 <43047067+jasonlee44@users.noreply.github.com>
2019-10-13 11:05:46 -0400

    Update AirQuality_API.py

--- Assignment 1/Final/AirQuality_API.py
+++ Assignment 1/Final/AirQuality_API.py
@@ -1,5 +1,8 @@
 import requests
 from tkinter import *
+import webbrowser
+import os
+import time
 
 def writeHTMLHead(): 
     global myfile
@@ -7,6 +10,7 @@ def writeHTMLHead():
     myfile.write("""<!DOCTYPE html>
     <html>
     <head>
+    <title>Air Quality API</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel = "stylesheet" type = "text/css" href = "style.css" />
     </head>
@@ -16,7 +20,7 @@ def writeHTMLNav():
     global myfile
     myfile.write("""    <body>
         <div class="header" id = "home">
-            <h1>Air Quality by Country</h1>
+            <h1>Air Quality by City</h1>
         </div>
         <div class="navbar">
             <a href="#home">Home</a>
@@ -62,27 +66,43 @@ china = ["Shanghai","Beijing","Chengdu","Hangzhou","Chongqing","Guangzhou","Shen
 england = ["London","Bristol","Liverpool","Manchester","Birmingham","Brighton","Oxford","Cambridge","Norwich"]
 germany = [""]
 
-countries_str = ["canada","united_states","china","england"] #list of countries
+countries_str = ["Canada","United States","China","England"] #list of countries
 countries = [canada,united_states,china,england] #list of list of cities
 
-def enter():
+def entry_enter(): #submiting a city
     global gui_input
     print(e1.get())
     gui_input = e1.get()
+    v.set("You will be redirected shortly")
+    time.sleep(1)
+    root.update()
     main()
     writeHTMLEnd()
-    print("Successful!")
+    webbrowser.open_new('file://' + os.getcwd() + '/Final/output.html')
+    v.set("")
+
+def menu_enter(): #sumbiting a country
+    global gui_input
+    print(variable.get())
+    gui_input = variable.get()
+    v.set("You will be redirected shortly")
+    time.sleep(1)
+    root.update()
+    main()
+    writeHTMLEnd()
+    webbrowser.open_new('file://' + os.getcwd() + '/Final/output.html')
+    v.set("")
 
 def main():
+    global labtext, myfile
     writeHTMLHead()
     writeHTMLNav()
     writeHTMLTableTag()
     
-    if gui_input in countries_str:
+    if gui_input in countries_str: #sumbiting a country
         for j in countries[countries_str.index(gui_input)]:
             response = requests.get(f"https://api.waqi.info/feed/{j}/?token=e6b58a3aa996953cceed553f70c04c07342ce32e")
             if (response.status_code == 200):
-                print("Status Code: 200")
                 data = response.json()
                 
                 if type(data['data']['aqi']).__name__ != "int":
@@ -90,6 +110,56 @@ def main():
                     health = "N/A"
                     statement = "N/A"
                     color = "grey"
+
+                else:
+                    if data['data']['aqi'] <= 50:
+                        level = "Good"
+                        health = "Air quality is considered satisfactory, and air pollution poses little or no risk"
+                        statement = "None"
+                        color = "rgb(51, 211, 51)"#green
+
+                    elif data['data']['aqi'] <= 100:
+                        level = "Moderate"
+                        health = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+                        statement = "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion."
+                        color = "rgb(255, 255, 100)"#yellow
+
+                    elif data['data']['aqi'] <= 150:
+                        level = "Unhealthy for Sensitive Groups"
+                        health = "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+                        statement = "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion."
+                        color = "rgb(245, 125, 45)"#orange
+
+                    elif data['data']['aqi'] <= 200:
+                        level = "Unhealthy"
+                        health = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
+                        statement = "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion"
+                        color = "rgb(140, 0, 0)" #red
+                        
+                    writeHTMLTable(j, data['data']['aqi'], level, health, statement, color)
+
+            else:
+                myfile.close()
+                myfile = open("./Final/output.html","w")
+                myfile.write("Error has occured")
+
+    else: #submiting a city
+        response = requests.get(f"https://api.waqi.info/feed/{gui_input}/?token=e6b58a3aa996953cceed553f70c04c07342ce32e")
+        if (response.status_code == 200):
+            data = response.json()
+
+            if data['status'] == 'error':
+                print("Error")
+                myfile.close()
+                myfile = open("./Final/output.html","w")
+                myfile.write("Error has occured")
+
+            else:
+                if type(data['data']['aqi']).__name__ != "int":
+                        level = "N/A"
+                        health = "N/A"
+                        statement = "N/A"
+                        color = "grey"
 
                 elif data['data']['aqi'] <= 50:
                     level = "Good"
@@ -115,61 +185,19 @@ def main():
                     statement = "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion"
                     color = "rgb(140, 0, 0)" #red
                     
-                writeHTMLTable(j, data['data']['aqi'], level, health, statement, color)
-
-            else:
-                myfile = open("./Assignment 1/myapi.html","w")
-                myfile.write("Error has occured")
-                myfile.close()
-    else:
-        response = requests.get(f"https://api.waqi.info/feed/{gui_input}/?token=e6b58a3aa996953cceed553f70c04c07342ce32e")
-        if (response.status_code == 200):
-            print("Status Code: 200")
-            data = response.json()
-
-            if type(data['data']['aqi']).__name__ != "int":
-                    level = "N/A"
-                    health = "N/A"
-                    statement = "N/A"
-                    color = "grey"
-
-            elif data['data']['aqi'] <= 50:
-                level = "Good"
-                health = "Air quality is considered satisfactory, and air pollution poses little or no risk"
-                statement = "None"
-                color = "rgb(51, 211, 51)"#green
-
-            elif data['data']['aqi'] <= 100:
-                level = "Moderate"
-                health = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
-                statement = "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion."
-                color = "rgb(255, 255, 100)"#yellow
-
-            elif data['data']['aqi'] <= 150:
-                level = "Unhealthy for Sensitive Groups"
-                health = "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
-                statement = "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion."
-                color = "rgb(245, 125, 45)"#orange
-
-            elif data['data']['aqi'] <= 200:
-                level = "Unhealthy"
-                health = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
-                statement = "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion"
-                color = "rgb(140, 0, 0)" #red
-                
-            writeHTMLTable(data['data']['city']['name'], data['data']['aqi'], level, health, statement, color)
+                writeHTMLTable(data['data']['city']['name'], data['data']['aqi'], level, health, statement, color)
+                print("Successful!")
         else:
-            myfile = open("./Assignment 1/myapi.html","w")
+            myfile = open("./Assignment 1/output.html","w")
             myfile.write("Error has occured")
             myfile.close()
-            
 
-
-
-
+#-------------GUI-------------
 root = Tk()
-root.title("photo tagger")
+root.title("Air Quality API")
 root.geometry("500x350")
+
+labtext = ""
 
 def test(event):
     print(event) #ensure that this line is indented
@@ -183,20 +211,42 @@ can1.create_line(0, 15, 20, 15,fill='white')
 can1.bind("<Button-1>",test ) # keyword 
 can1.grid(row=0,column=0, padx=5, pady=5)
 
-spaceframe = Frame(root,height=10) #invisible
+spaceframe = Frame(root,height=20) #invisible frame
 spaceframe.pack()
 
-frame = LabelFrame(root,borderwidth = 2, relief=RAISED, width=400,height=150)
-frame.pack()
+frame1 = LabelFrame(root,borderwidth = 2, relief=RAISED, width=400,height=150)
+frame1.pack()
 
-lab1 = Label(frame, text="fjkdsjkfsdjkfasdkfjkasd fdsakjfasj fkasjlf",width=40,height=5)
+lab1 = Label(frame1, text="Enter a city",width=30,height=0)
 lab1.grid(row=0,column=0)
 
-e1 = Entry(frame)
-e1.grid(row=1,column=0,sticky=W,padx=60)
+e1 = Entry(frame1)
+e1.grid(row=1,column=0,sticky=W,padx=10)
 
-b = Button(frame, text="Sumbit",command=enter)
-b.grid(row=1, column=0,sticky=E,padx=60)
+b1 = Button(frame1, text="Sumbit",command=entry_enter)
+b1.grid(row=1, column=0,sticky=E,padx=10)
+
+lab2 = Label(root,text="- OR -")
+lab2.pack(pady=10)
+
+frame2 = LabelFrame(root,borderwidth = 2, relief=RAISED, width=400,height=150)
+frame2.pack(anchor=CENTER)
+
+variable = StringVar(frame2)
+variable.set("    ") # default value
+
+m1 = OptionMenu(frame2, variable, "Canada", "United States", "China","England")
+m1.config(width=17)
+m1.grid(row=1,column=0,sticky=W,padx=10)
+
+lab3 = Label(frame2, text="Select a country",width=30)
+lab3.grid(row=0,column=0)
+
+b2 = Button(frame2, text="Sumbit",command=menu_enter)
+b2.grid(row=1, column=0,sticky=E,padx=10)
+
+v = StringVar()
+lab4 = Label(root, textvariable=v)
+lab4.pack()
 
 root.mainloop()
-
